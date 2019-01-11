2629113fde6e547c5094d5810306f9916792d4f0
Abhishek Chakraborty <chakraborty@silexica.com>
2019-01-11 21:15:23 +0100

    Add complete example for Sprintboot with eureka and ribbon

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # springboot
+
+
+
+#Run MySql Docker container
+docker run -p 6603:3306 --name=docker-mysql --env="MYSQL_ROOT_PASSWORD=root" --env="MYSQL_PASSWORD=root" --env="MYSQL_DATABASE=test" mysql
+
+docker run --name=docker-mysql --env="MYSQL_ROOT_PASSWORD=root" --env="MYSQL_PASSWORD=root" --env="MYSQL_DATABASE=test" mysql
+
+#Build docker file
+docker build -f Dockerfile -t gradle-springboot-docker .
+
+#Run springboot docker file
+docker run -t --name gradle-springboot-docker --link docker-mysql:mysql -p 8080:8080 gradle-springboot-docker
+
---
+++ forex-microservices/com.example.currency-conversion/src/main/java/com/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,17 @@
+package com.example.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients("com.example.currencyconversion")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+	}
+
+}
