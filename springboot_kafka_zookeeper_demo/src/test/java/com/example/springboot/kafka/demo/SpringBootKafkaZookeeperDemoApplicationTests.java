6a50a647f9eb427e6a5f15342af38e4e9d8b314a
Chakraborty <chakraborty@silexica.com>
2019-02-10 15:45:55 +0100

    Sample kafka app

---
+++ springboot_kafka_zookeeper_demo/src/test/java/com/example/springboot/kafka/demo/SpringBootKafkaZookeeperDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springboot.kafka.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootKafkaZookeeperDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
