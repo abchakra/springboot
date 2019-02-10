184bdb101fde0c3a9e13b186f35d944f321adc56
Abhishek Chakraborty <chakraborty@silexica.com>
2019-02-10 12:24:18 +0100

    Using external API

---
+++ SpringCloud/Person-Service/src/main/java/com/springcloud/example/person/service/PersonService/PersonServiceApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.example.person.service.PersonService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonServiceApplication.class, args);
+	}
+
+}
+
---
+++ SpringCloud/Person-Service/src/main/java/com/springcloud/example/person/service/PersonService/model/Person.java
@@ -0,0 +1,10 @@
+package com.springcloud.example.person.service.PersonService.model;
+
+public class Person {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+//    private Gender gender;
+}
