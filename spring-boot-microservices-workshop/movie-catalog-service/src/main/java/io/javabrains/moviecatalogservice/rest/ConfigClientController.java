212258143b0a98045c25967cfdf5889e4657f561
Chakraborty <chakraborty@silexica.com>
2019-04-21 00:01:26 +0200

    Add Cloud Configuration for centeralized configuration

---
+++ spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/rest/ConfigClientController.java
@@ -0,0 +1,18 @@
+package io.javabrains.moviecatalogservice.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+	@Value("${user.role}")
+	private String role;
+
+	@GetMapping("/profile/{name}")
+	public String getActiveProfile(@PathVariable String name) {
+		return "Hello " + name + "! active profile name is " + role;
+	}
+}
